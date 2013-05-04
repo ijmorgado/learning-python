6622aad355cebabf098d1c3f727ffbf7f6920180
ijmorgado <i.jimenez.morgado@gmail.com>
2013-05-03 23:24:43 -0500

    Inheritance

---
+++ inheritance/main.py
@@ -0,0 +1,31 @@
+
+
+class Person(object):
+	def __init__(self):
+		self.name = 'Jose'
+		self.age = 18
+		self.listItems = ['one','two']
+
+	def sayHello(self):
+		print "Hello " + self.name
+
+	def printItems(self):
+		for item in self.listItems:
+			print item
+
+
+class Worker(Person):
+	def __init__(self):
+		self.position = 'Lawyer'
+		super(Worker, self).__init__()
+
+	def work(self):
+		print "I'm already working as " + self.position
+
+w = Worker()
+w.work()
+w.sayHello()
+w.printItems()
+w.listItems.append('tree')
+print "After added...."
+w.printItems()
